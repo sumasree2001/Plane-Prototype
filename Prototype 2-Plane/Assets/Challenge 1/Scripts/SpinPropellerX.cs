8f8a849f88c66b507583f7e2761be2bec1977d08
sumasree2001 <48171322+sumasree2001@users.noreply.github.com>
2023-09-07 22:05:01 -0700

    plane prototype

---
+++ Prototype 2-Plane/Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{   public float rotationSpeed=25.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward,rotationSpeed * Time.deltaTime);
+    }
+}
